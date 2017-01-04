88d7ac1ffc414805b2ac053f2040fc11f9eef3c7
Admin <admin@Admins-MacBook-Pro-3.local>
2017-01-03 21:49:00 -0800

    Initial Commit

---
+++ ArrayClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArrayClass
+//
+//  Created by Admin on 1/3/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
